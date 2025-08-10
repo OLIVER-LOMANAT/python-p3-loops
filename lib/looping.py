6237d0e1f78406d90239460d4a2e4ea4bd3f3ff5
Oliver Lomanat <lomanatoliver@gmail.com>
2025-08-10 05:16:25 +0300

    applying control flow

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,54 @@
+# #!/usr/bin/env python3
+
+# def happy_new_year():
+#     number = 10
+#     while(number > 0):
+#         print(number)
+#         number -=1
+#     print("Happy New Year!")
+
+# def square_integers(int_list):
+#     square_list = []
+#     for num in int_list:
+#         square_list.append(num ** 2)
+#         return square_list
+    
+# def fizzbuzz():
+#     for num in range (1, 101):
+
+#         if num % 3 == 0 and num % 5 == 0:
+#             print("FizzBuzz")
+#         elif num % 3 == 0:
+#             print("Buzz")
+#         elif num % 5 ==0:
+#             print("Fizz")
+#         else:
+#             print(num)
+        
+
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    number = 10
+    while(number > 0):
+        print(number)
+        number -= 1
+    print("Happy New Year!")
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    squared_list = []
+    for num in int_list:
+        squared_list.append(num ** 2)
+    return squared_list
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for num in range(1, 101):
+        if num % 3 == 0 and num % 5 == 0:
+            print("FizzBuzz")
+        elif num % 3 == 0:
+            print("Fizz")
+        elif num % 5 == 0:
+            print("Buzz")
+        else:
+            print(num)
+
